edb4b43d331e3596d07de6f0862ae0bbfb1d4a80
Max Kuznetsov <maks.kuznetsov@gmail.com>
2020-06-01 03:47:59 +0300

    dl.py: `download` supports multiple URLs and skips live streams

--- python/dl.py
+++ python/dl.py
@@ -4,7 +4,6 @@ import argparse
 import contextlib
 import copy
 import datetime as dt
-import glob
 import json
 import logging
 import os
@@ -169,7 +168,7 @@ class Download:
         os.makedirs(TEMP_ROOT, exist_ok=True)
         self.output_dir = tempfile.mkdtemp(dir=TEMP_ROOT)
 
-        self.url: str = args.url
+        self.urls: typing.List[str] = args.urls
 
         logger = None
         if args.log:
@@ -189,40 +188,57 @@ class Download:
         import youtube_dl
 
         ydl = youtube_dl.YoutubeDL(self.opts)
-        info = ydl.extract_info(self.url)
+        process_info = ydl.process_info
+
+        infos = {}
+
+        def process_hook(data):
+            if not data.get('id') or data.get('is_live'):
+                return
+            infos[data['id']] = data
+            return process_info(data)
+
+        with mock.patch.object(ydl, 'process_info', process_hook):
+            ydl.download(self.urls)
 
         os.chdir(self.output_dir)
 
-        output_dirs = glob.glob('*')
-        if not output_dirs:
-            raise Error('could not find output dir')
+        result = []
 
-        zip_path = f"{info['upload_date']}_{info['id']}.zip"
+        for info in infos.values():
+            video_dir = f"{info['upload_date']}_{info['id']}"
+            if not os.path.exists(video_dir):
+                continue
 
-        r = sp.run(
-            ['zip', '-q', '-0', '-r', zip_path, *output_dirs],
-            stdout=sp.PIPE, stderr=sp.STDOUT
-        )
-        if r.returncode:
-            raise Error(r.stdout.decode().strip())
+            zip_filename = video_dir + '.zip'
+            r = sp.run(
+                ['zip', '-q', '-0', '-r', zip_filename, video_dir],
+                stdout=sp.PIPE, stderr=sp.STDOUT
+            )
+            if r.returncode:
+                raise Error(r.stdout.decode().strip())
 
-        for d in output_dirs:
-            shutil.rmtree(d, ignore_errors=True)
+            shutil.rmtree(video_dir, ignore_errors=True)
 
-        upload_date = dt.datetime.strptime(info['upload_date'], '%Y%m%d').date()
+            upload_date = dt.datetime.strptime(info['upload_date'], '%Y%m%d').date()
 
-        # Remove redundant keys
-        for key in ('formats', 'requested_formats', 'format', 'format_id'):
-            info.pop(key, None)
+            redundant_keys = (
+                'formats', 'requested_formats', 'format', 'format_id',
+                *(k for k in info if str(k).startswith('_')),
+            )
+            for key in redundant_keys:
+                info.pop(key, None)
 
-        return {
-            'id': info['id'],
-            'title': info.get('title', '<missing title>'),
-            'uploader': info.get('uploader', '<unknown uploader>'),
-            'upload_date': upload_date.isoformat(),
-            'file': os.path.join(self.output_dir, zip_path),
-            'info': info,
-        }
+            result.append({
+                'id': info['id'],
+                'title': info.get('title', '<missing title>'),
+                'uploader': info.get('uploader', '<unknown uploader>'),
+                'upload_date': upload_date.isoformat(),
+                'file': os.path.join(self.output_dir, zip_filename),
+                'info': info,
+            })
+
+        return result
 
 
 def arg_parser():
@@ -237,7 +253,7 @@ def arg_parser():
     subcmd.set_defaults(func=Download)
     subcmd.add_argument('--log')
     subcmd.add_argument('--preset', choices=['video', 'audio'], default='video')
-    subcmd.add_argument('url')
+    subcmd.add_argument('urls', nargs='+')
 
     return parser
 
