77d0d83cb50f4c4c08e4fa0536636475bc2d9493
Max Kuznetsov <maks.kuznetsov@gmail.com>
2020-06-02 22:56:39 +0300

    sql: add filehash and attempts columns

--- sql/0001_init.up.sql
+++ sql/0001_init.up.sql
@@ -1,14 +1,20 @@
-CREATE TABLE ytbackup
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE videos
 (
-    id       INTEGER PRIMARY KEY ASC AUTOINCREMENT,
-    video_id TEXT UNIQUE NOT NULL,
-    title    TEXT,
-    uploader TEXT,
-    info     JSON,
-    status   TEXT CHECK (status IN ('NEW', 'SKIPPED', 'FAILED', 'DOWNLOADED')) DEFAULT 'NEW',
-    volume   TEXT,
-    path     TEXT,
-    filesize INTEGER
+    id          INTEGER PRIMARY KEY ASC AUTOINCREMENT,
+    video_id    TEXT UNIQUE NOT NULL,
+    title       TEXT,
+    uploader    TEXT,
+    upload_date DATE,
+    info        JSON,
+    status      TEXT CHECK (status IN ('NEW', 'SKIPPED', 'FAILED', 'DOWNLOADED'))
+                        DEFAULT 'NEW',
+    volume      TEXT,
+    path        TEXT,
+    filesize    INTEGER,
+    filehash    TEXT,
+    attempts    INTEGER DEFAULT 0
 );
 
-CREATE INDEX ytbackup__status_idx ON ytbackup (status);
+CREATE INDEX ytbackup__status_idx ON videos (status);
